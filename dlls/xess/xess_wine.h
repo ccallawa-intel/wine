9b665e4f985a66431fbb4836b162d5d3f7921d72
Caleb Callaway <caleb.callaway@intel.com>
2026-01-22 19:01:11 +0000

    WIP XeSS translation

---
+++ dlls/xess/xess_wine.h
@@ -0,0 +1,34 @@
+/*
+ * XeSS DLL - Internal definitions
+ * Copyright 2025 the Wine project
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ */
+
+#ifndef __WINE_XESS_INTERNAL_H
+#define __WINE_XESS_INTERNAL_H
+
+#include "wine/vulkan.h"
+#include "wine/vulkan_driver.h"
+
+/* stdbool must be included _before_ the XeSS headers,
+ or the Wine build will complain about undefined types */
+#include <stdbool.h>
+#include "extern/xess.h"
+#include "extern/xess_debug.h"
+
+// HACK: disable vulkan include in xess_vk.h
+// without modifying the header itself
+// so the header can be copied directly from upstream
+#define VULKAN_CORE_H_ 1
+#include "extern/xess_vk.h"
+#undef VULKAN_CORE_H_
+
+const char* xess_result_to_string(xess_result_t result);
+
+void xess_d3d12_clear_init_heap_store(void);
+
+#endif /* __WINE_XESS_INTERNAL_H */
