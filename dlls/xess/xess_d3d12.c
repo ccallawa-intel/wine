5729eb4f277dcc8e82eeece7e82b60128680267b
Caleb Callaway <caleb.callaway@intel.com>
2026-06-29 17:05:55 -0700

    More Copilot review fixes

--- dlls/xess/xess_d3d12.c
+++ dlls/xess/xess_d3d12.c
@@ -431,6 +431,9 @@ xess_result_t CDECL xessD3D12Execute(xess_context_handle_t hContext,
 
     TRACE("(%p, %p, %p)\n", hContext, pCommandList, pExecParams);
 
+    if (!pExecParams)
+        return XESS_RESULT_ERROR_INVALID_ARGUMENT;
+
     memset(&vk_exec_params, 0, sizeof(vk_exec_params));
     memset(&unix_params, 0, sizeof(unix_params));
 
